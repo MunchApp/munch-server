be9f9f0ed6791696276b87b4d76de8e648fa36d4
Majjalpee <mazharnaqvi786@gmail.com>
2019-10-24 17:57:56 -0500

    Think I fixed name addressing

--- gqlfields/model-gql-fields.go
+++ gqlfields/model-gql-fields.go
@@ -6,8 +6,19 @@ import "github.com/graphql-go/graphql"
 var UserType = graphql.NewObject(graphql.ObjectConfig{
 	Name: "userFields",
 	Fields: graphql.Fields{
-		"name":  &graphql.Field{Type: graphql.String},
+		"firstName":  &graphql.Field{Type: graphql.String},
+		"lastName":  &graphql.Field{Type: graphql.String},
 		"email": &graphql.Field{Type: graphql.String},
+		"id":    &graphql.Field{Type: graphql.String},
+	},
+})
+
+// GQL Fields to RETURN for foodTruck
+var foodTruckType = graphql.NewObject(graphql.ObjectConfig{
+	Name: "foodTruckFields",
+	Fields: graphql.Fields{
+		"name":  &graphql.Field{Type: graphql.String},
+		"": &graphql.Field{Type: graphql.String},
 		"id":    &graphql.Field{Type: graphql.String},
 	},
 })
