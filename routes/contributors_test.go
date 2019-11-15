26d4b979e60e5bb6088d51f2c83db6bbaa5d9fb1
kftang <kennyftang@gmail.com>
2019-11-14 18:50:03 -0600

    Added a test for contributors

---
+++ routes/contributors_test.go
@@ -0,0 +1,21 @@
+package routes
+
+import (
+	"munchserver/tests"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestContributorsGet(t *testing.T) {
+	tests.ClearDB()
+	req, _ := http.NewRequest("GET", "/contributors", nil)
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(GetContributorsHandler)
+	handler.ServeHTTP(rr, req)
+
+	expected := http.StatusOK
+	if rr.Code != expected {
+		t.Errorf("getting contributors expected status code of %v, but got %v", expected, rr.Code)
+	}
+}
